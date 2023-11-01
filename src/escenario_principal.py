8ae4f244ea1b559ad1d709d58db6e37ec8e72f8e
Juan <jpledezma01@gmail.com>
2023-10-31 22:42:28 -0300

    Se agregan posiciones aleatorias a los enemigos

--- src/escenario_principal.py
+++ src/escenario_principal.py
@@ -6,6 +6,7 @@ from config import *
 from calculos import *
 from utilidades import *
 from menus import *
+from random import randint
 
 # ------------------  Menu Principal  ------------------
 def escenario_juego(pantalla:pygame.Surface):
@@ -51,7 +52,11 @@ def escenario_juego(pantalla:pygame.Surface):
     # Texto segundos
     texto_contador_segundos = escribir_texto((0, 0), f"{temporizador:03}", AMARILLO)
     texto_contador_segundos['rect'].center = (ancho_pantalla //2, altura_hud // 2)
-    
+
+    # Contador enemigos derrotados
+    enemigos_derrotados = 0
+    texto_contador_enemigos = escribir_texto((0, 0), f"Enemigos derrotados: {enemigos_derrotados:02}", AMARILLO)
+    texto_contador_enemigos['rect'].center = (ancho_pantalla // 4 * 3, altura_hud // 2)
     # Música del juego
     pygame.mixer.music.load("assets\musica\OurLordIsNotReady.mp3")
     if musica_activa:
@@ -59,10 +64,18 @@ def escenario_juego(pantalla:pygame.Surface):
     
 
     # Personaje principal
-    img_personaje = pygame.image.load("assets/sprites/personaje/knight_idle_0.png")
+    # Atributos
+    multiplicador_sprint = 1
     pj_vida_maxima = 200
     pj_mana_maxima = 100
     pj_energia_maxima = FPS * 3
+    pj_vulnerable = True
+    pj_dañado = False
+    iframes = FPS #// 2
+    cooldown_iframes = iframes
+    pj_daño_recibido = 0
+    # Imagen
+    img_personaje = pygame.image.load("assets/sprites/personaje/knight_idle_0.png")
     personaje = crear_entidad((0, 0), 90, 70, vida=pj_vida_maxima, poder_ataque=20, mana=pj_mana_maxima, energia=pj_energia_maxima, imagen=img_personaje)
     personaje['rect'].center = (ancho_pantalla / 2, alto_pantalla - 50)
     personaje['velocidad'] = 1
@@ -73,8 +86,7 @@ def escenario_juego(pantalla:pygame.Surface):
     pj_mover_derecha = False
     pj_mover_izquierda = False
     pj_sprint = False
-    multiplicador_sprint = 1
-    # Animaciones personaje
+    # Animaciones
     animaciones = {'quieto': [], 'moviendo_arriba':[], 'moviendo_abajo':[], 'moviendo_izquierda':[], 'moviendo_derecha':[]}
     for i in range(4):
         animaciones['quieto'].append(crear_superficie((0, 0), 90, 70, imagen=pygame.image.load(f"assets/sprites/personaje/knight_idle_{i}.png")))
@@ -86,14 +98,24 @@ def escenario_juego(pantalla:pygame.Surface):
     pj_animacion_seleccionada = 'quieto'
 
     # Enemigos
-    img_zombie = img_personaje = pygame.image.load("assets/sprites/enemigos/zombie.png")
-    zombie = crear_entidad((400, 400), 75, 100, 100, 10, imagen=img_zombie)
-    img_esqueleto = img_personaje = pygame.image.load("assets/sprites/enemigos/skeleton.png")
-    esqueleto = crear_entidad((500, 400), 70, 100, 100, 10, imagen=img_esqueleto)
+    img_zombie = pygame.image.load("assets/sprites/enemigos/zombie.png")
+    img_esqueleto = pygame.image.load("assets/sprites/enemigos/skeleton.png")
+    tipos_enemigos = [img_zombie, img_esqueleto]
 
-    enemigos = [zombie, esqueleto]
+    # enemigos = [zombie, esqueleto]
+    enemigos = []
+    for i in range(8):
+        enemigo = crear_entidad((0, 0), ancho=75, alto=100, vida=randint(80, 240), poder_ataque=randint(20, 50), imagen=tipos_enemigos[randint(0, 1)])
+        enemigo['rect'].right = randint(limite_izquierdo, limite_derecho)
+        enemigo['rect'].top = randint(limite_superior, limite_inferior//2)
+        enemigos.append(enemigo)
+    # entidades = [zombie, esqueleto, personaje]
+    entidades = enemigos.copy()
+    entidades.append(personaje)
 
-    entidades = [zombie, esqueleto, personaje]
+    for entidad in entidades:
+        entidad['hitbox'].bottom = entidad['rect'].bottom
+        entidad['hitbox'].centerx = entidad['rect'].centerx
 
     # Loop del juego
     while True:
@@ -139,7 +161,34 @@ def escenario_juego(pantalla:pygame.Surface):
                 if evento.key == K_LSHIFT:
                     pj_sprint = False
                 
+
+
+        for enemigo in enemigos:
+            if calcular_distancia(enemigo['rect'].center, personaje['rect'].center) <= enemigo['radio_deteccion']:
+                enemigo['agresivo'] = True
+
+            if personaje['hitbox'].colliderect(enemigo['hitbox']) and pj_vulnerable:
+                pj_dañado = True
+                pj_daño_recibido = enemigo['poder_ataque']
+
+            # Si el jugador se aleja mucho, el enemigo deja de perseguirlo
+            if calcular_distancia(enemigo['rect'].center, personaje['rect'].center) > enemigo['radio_deteccion'] * 3:
+                enemigo['agresivo'] = False
+
+                
         # ------ Actualizar elementos ------
+        # Daño al personaje
+        if pj_dañado:
+            pj_dañado = False
+            personaje['vida'] -= pj_daño_recibido
+            pj_vulnerable = False
+
+        if not pj_vulnerable and cooldown_iframes > 0:
+            cooldown_iframes -= 1
+        else:
+            cooldown_iframes = iframes
+            pj_vulnerable = True
+
 
         # Movimiento personaje
 
@@ -193,12 +242,10 @@ def escenario_juego(pantalla:pygame.Surface):
 
 
         # Movimiento enemigos
-        for enemigo in enemigos:
-            if calcular_distancia(enemigo['rect'].center, personaje['rect'].center) <= enemigo['radio_deteccion']:
-                enemigo['agresivo'] = True
 
         for enemigo in enemigos:
             if enemigo['agresivo']:
+                # Mover al enemigo hacia el jugador
                 if enemigo['rect'].centery > personaje['rect'].centery:
                     enemigo['rect'].centery -= enemigo['velocidad']
                 else:
@@ -207,9 +254,6 @@ def escenario_juego(pantalla:pygame.Surface):
                     enemigo['rect'].centerx -= enemigo['velocidad']
                 else:
                     enemigo['rect'].centerx += enemigo['velocidad']
-
-                if calcular_distancia(enemigo['rect'].center, personaje['rect'].center) > enemigo['radio_deteccion'] * 3:
-                    enemigo['agresivo'] = False
 
 
         for entidad in entidades:
@@ -228,7 +272,7 @@ def escenario_juego(pantalla:pygame.Surface):
         barra_vida['rect'].midleft = (fondo_barra_vida['rect'].left + 5, fondo_barra_vida['rect'].centery)
 
 
-        # Dibujar elementos
+        # ------ Dibujar elementos ------
 
         # Escenario
         pantalla.blit(fondo_escenario, (0, altura_hud))
@@ -236,6 +280,7 @@ def escenario_juego(pantalla:pygame.Surface):
 
         # HUD
         blitear_texto(pantalla, texto_contador_segundos)
+        blitear_texto(pantalla, texto_contador_enemigos)
 
         dibujar_rectangulo(pantalla, fondo_barra_vida)
         dibujar_rectangulo(pantalla, barra_vida)
@@ -252,24 +297,21 @@ def escenario_juego(pantalla:pygame.Surface):
         pantalla.blit(animaciones[pj_animacion_seleccionada][frame_animacion_seleccionada]['superficie'], personaje['rect'])
 
         # Hitboxes
-        pygame.draw.rect(pantalla, BLANCO, zombie['rect'], 1)
-        pygame.draw.rect(pantalla, BLANCO, esqueleto['rect'], 1)
-        pygame.draw.rect(pantalla, BLANCO, zombie['hitbox'], 1)
-        pygame.draw.rect(pantalla, BLANCO, esqueleto['hitbox'], 1)
-        pygame.draw.rect(pantalla, ROJO, personaje['hitbox'], 1)
+        for entidad in entidades:
+            pygame.draw.rect(pantalla, BLANCO, entidad['hitbox'], 1)
+        # pygame.draw.rect(pantalla, BLANCO, zombie['rect'], 1)
+        # pygame.draw.rect(pantalla, BLANCO, esqueleto['rect'], 1)
+        # pygame.draw.rect(pantalla, BLANCO, zombie['hitbox'], 1)
+        # pygame.draw.rect(pantalla, BLANCO, esqueleto['hitbox'], 1)
+        # pygame.draw.rect(pantalla, ROJO, personaje['hitbox'], 1)
 
         
-
-
         pygame.display.flip()
 
 # pygame.init()
 # escenario_juego(pygame.display.set_mode(TAMAÑO_PANTALLA))
 
 # TODO 
-# agregar menu de pausa
 # agregar eventos personalizados (que aparezca una bruja cada cierto tiempo, hasta 3 veces)     
-# agregar iframes
-# agregar colisiones con enemigos
 # Agregar ataques
 # Agregar objetos consumibles (vida, mana)
